de6a2927471e9732db634dc4f011985240f9a77c
Nikolay Punko <punkon2001@gmail.com>
2023-10-16 15:40:07 +0300

    add sleep for testing

--- src/main/java/com/example/SpringTestGraalVM/service/ScriptService.java
+++ src/main/java/com/example/SpringTestGraalVM/service/ScriptService.java
@@ -12,6 +12,11 @@ public class ScriptService {
     public int getMethod1() {
         ScriptContext context = ContextPool.borrowContext();
         int result = PythonConfiguration.getPythonService(context).testMethod_1(5, 10);
+        try {
+            Thread.sleep(50);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
         ContextPool.returnContext(context);
         return result;
     }
@@ -19,6 +24,11 @@ public class ScriptService {
     public String getMethod2() {
         ScriptContext context = ContextPool.borrowContext();
         String result = PythonConfiguration.getPythonService(context).testMethod_2();
+        try {
+            Thread.sleep(50);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
         ContextPool.returnContext(context);
         return result;
     }
@@ -26,6 +36,11 @@ public class ScriptService {
     public String getMethod3() {
         ScriptContext context = ContextPool.borrowContext();
         String result = PythonConfiguration.getPythonService(context).testMethod_3(new Person("Ivan", 35));
+        try {
+            Thread.sleep(50);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
         ContextPool.returnContext(context);
         return result;
     }
@@ -33,6 +48,11 @@ public class ScriptService {
     public Person getMethod4() {
         ScriptContext context = ContextPool.borrowContext();
         Person result = PythonConfiguration.getPythonService(context).testMethod_4(new Person("Ivan", 35));
+        try {
+            Thread.sleep(50);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
         ContextPool.returnContext(context);
         return result;
     }
